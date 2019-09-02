2bb856e684fe100ef554ff53e0a6812a1569adb7
Howcanoe WANG <howcanoewang@gmail.com>
2019-09-02 12:59:26 -0300

    Setup UI
    只是把一层皮做好了= =，功能还没开始搞

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # AkrnightsSimulatorHelper.exe
-Arknights Simulator Auto Helper | 基于python鼠标控制的明日方舟模拟器护肝助手
+Arknights Simulator Helper | 基于python鼠标控制的明日方舟模拟器护肝助手
 
-QRegion block mouse events
-https://stackoverflow.com/questions/46940966/pyqt-how-to-set-a-mask-over-a-qwidget-to-ignore-mouse-events-but-get-the-childr
+![](img/md_gui.png)
 
-QMask to set transparent
+## 特性
 
-动态生成Mask
-https://www.cnblogs.com/codeio/archive/2011/09/02/2164017.html
+* 采用相对坐标，兼容各类模拟器
+* 控制鼠标进行挂机操作(运行时请不要操作电脑，适合睡前点开挂机)
+* 无需安装安卓ADB工具和Python各种包，exe文件点开即用
+* 脚本操作与程序分离，将脚本文件拷到Scripts文件夹内即可运行
+* **以上功能均没有实现**
+
+## Todo
+* [x] 界面制作
+* [ ] 相对坐标坐标系计算
+* [ ] 图片匹配
+* [ ] 配置文件读写(记住目前模拟器的尺寸)
+* [ ] 脚本解释器
+* [ ] 脚本编辑器
+
+## 脚本说明
+脚本采用以.ark为后缀的文件夹，里面包含img文件夹，run脚本+说明文件组成。img文件夹存放该脚本需要用到的截图，如开始，返回等按钮的截图等，类似于sikulix
+
+![](img/md_script.png)
+
+需要在程序内部集成运行解释器，还需要另写一个脚本编辑器方便大家自己写脚本
+
+> 为什么不直接用sikulix? 因为想搞一个绿色的exe，不需要安装额外的依赖，减少使用难度
--- app.py
+++ app.py
@@ -1,61 +1,228 @@
 # -*- coding:utf-8 -*-
 import sys
+import time
+import pyautogui as pag
+from random import randrange
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-class Helper(QMainWindow):
 
-    '''
+class Helper(QWidget):
+    dirty = True
+    drag = False
+    title = '明日方舟护肝助手V0.1'
+
     def __init__(self, parent=None):
         super().__init__(parent)
-        
-        self.setWindowTitle('明日方舟护肝小助手')
+        self.screen_ref = min(screen_height, screen_width)
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowCloseButtonHint | Qt.WindowStaysOnTopHint)
         self.resize(1280, 720)
-        #self.setAttribute(Qt.WA_TranslucentBackground)
-        #self.setWindowFlags(Qt.FramelessWindowHint)
-        #self.setStyleSheet("background:transparent;")
-        #self.setStyleSheet("background-color: rgba(100,0,0,0);")
-        self.setWindowOpacity(0.5)
-        #self.setAttribute(Qt.WA_TransparentForMouseEvents)
-        #self.setFocusPolicy(Qt.NoFocus)
-        #self.setWindowFlags(Qt.WindowTransparentForInput | Qt.WindowStaysOnTopHint)
-        self.setWindowFlags(Qt.WindowStaysOnTopHint)
-        
-        self.mainWidget = QWidget()
-        self.mainWidget.setWindowFlags(Qt.WindowTransparentForInput | Qt.WindowStaysOnTopHint)
-        self.mainWidget.setStyleSheet("background:transparent;")
-        self.setCentralWidget(self.mainWidget)
-    '''
-    def __init__(self, parent=None):
-        
-        super().__init__(parent)
-        
-        self.setWindowTitle('明日方舟护肝小助手')
-        self.resize(1280, 720)
-        
-        self.setWindowFlags(Qt.CustomizeWindowHint | Qt.FramelessWindowHint)
-        self.setStyleSheet("border: 1px solid rgba(0, 0, 0, 0.15);")
-        self.setSizeGripEnabled(True);
-        
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+        self.setContentsMargins(0, 0, 0, 0)
+        self.setWindowTitle(self.title)
+        self.setWindowIcon(QIcon('img/images.jpg'))
+        self.setMouseTracking(True)
+
+        self.setupUI()
+        self.functionConnector()
+
+    ##############
+    # GUI Design #
+    ##############
+    def setupUI(self):
+        self.mainlayout = QVBoxLayout(self)
+        self.setLayout(self.mainlayout)
+        self.mainlayout.setContentsMargins(0, 0, 0, 0)
+        self.mainlayout.setSpacing(0)
+
+        # ======= remake window title========
+        self.winTitle = QHBoxLayout(self)
+
+        self.iconLabel = QLabel(self)
+        self.titleLabel = QLabel(self)
+        self.minButton = QPushButton(self)
+        self.closeButton = QPushButton(self)
+
+        self.iconLabel.setAlignment(Qt.AlignLeft)
+        self.titleLabel.setAlignment(Qt.AlignCenter)
+
+        self.iconLabel.setPixmap(QPixmap("img/images.jpg").scaled(self.screen_ref*0.03, self.screen_ref*0.03))
+        self.titleLabel.setText(self.title)
+        self.titleLabel.setFont(QFont('Microsoft YaHei UI', 15))
+        self.minButton.setIcon(QIcon("img/minimize.png"))
+        self.minButton.setIconSize(QSize(self.screen_ref*0.03, self.screen_ref*0.03))
+        self.minButton.setStyleSheet("QPushButton{border:none};")
+        self.closeButton.setIcon(QIcon("img/cancel.png"))
+        self.closeButton.setIconSize(QSize(self.screen_ref * 0.03, self.screen_ref * 0.03))
+        self.closeButton.setStyleSheet("QPushButton{border:none};")
+
+        # screen info on wintitle
+        self.infoLayout = QVBoxLayout()
+
+        self.frameInfoLayout = QHBoxLayout()
+        self.frameHtLabel = QLabel('高[720]像素')
+        self.frameWdLabel = QLabel('宽[1280]像素')
+        self.frameInfoLayout.addWidget(self.frameWdLabel)
+        self.frameInfoLayout.addWidget(self.frameHtLabel)
+        self.infoLayout.addLayout(self.frameInfoLayout)
+
+        self.mouseInfoLayout = QHBoxLayout()
+        self.mouseRefPos = QLabel('(0.50, 0.50)')
+        self.mouseInfoLayout.addWidget(QLabel('鼠标相对坐标:'))
+        self.mouseInfoLayout.addWidget(self.mouseRefPos)
+        self.infoLayout.addLayout(self.mouseInfoLayout)
+
+        # combine widgets
+        self.winTitle.addWidget(self.iconLabel)
+        self.winTitle.addWidget(self.titleLabel)
+        self.winTitle.addLayout(self.infoLayout)
+        self.winTitle.addStretch()
+        self.winTitle.addWidget(self.minButton)
+        self.winTitle.addWidget(self.closeButton)
+
+        self.mainlayout.addLayout(self.winTitle)
+
+        # ======== set transparent simulator control ==========
+        self.simulatorWidget = QWidget()
+        self.simulatorWidget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.mainlayout.addWidget(self.simulatorWidget)
+
+        # ======== set bottom tools ===========
+        self.toolLayout = QHBoxLayout()
+        #self.toolLayout.setContentsMargins(5, 5, 5, 5)
+
+        # Script Choice
+        self.scriptChoiceLayout = QVBoxLayout()
+        self.scriptChoice = QComboBox()
+        # Todo: Load Configure to items
+        self.scriptChoice.addItems(['OF_7', 'OF_F4'])
+        self.scriptChoice.setFixedHeight(int(self.screen_ref*0.02))
+
+        self.scriptChoiceLayout.addWidget(QLabel('选择辅助脚本：'))
+        self.scriptChoiceLayout.addWidget(self.scriptChoice)
+
+        self.toolLayout.addLayout(self.scriptChoiceLayout)
+        self.toolLayout.addWidget(VLine())
+
+        # Time Input
+        self.loopTimeLayout = QVBoxLayout()
+        self.loopTime = QSpinBox()
+        self.loopTime.setRange(0, 30)
+        self.loopTime.setValue(0)
+        self.loopTime.setFixedHeight(int(self.screen_ref * 0.02))
+
+        self.loopTimeLayout.addWidget(QLabel('执行次数：'))
+        self.loopTimeLayout.addWidget(self.loopTime)
+
+        self.toolLayout.addLayout(self.loopTimeLayout)
+
+        # Start Btn
+        self.startButton = QPushButton('开始')
+        self.startButton.setFixedHeight(int(self.screen_ref*0.04))
+
+        self.toolLayout.addWidget(self.startButton)
+
+        self.toolLayout.addStretch()
+
+        self.sizegrip = QSizeGrip(self)
+        self.toolLayout.addWidget(self.sizegrip, 0, Qt.AlignBottom | Qt.AlignRight)
+
+        self.mainlayout.addLayout(self.toolLayout)
+
+    def functionConnector(self):
+        self.minButton.clicked.connect(self.showMininizedWindow)
+        self.closeButton.clicked.connect(self.closeWindow)
+        self.scriptChoice.currentIndexChanged.connect(self.scriptChanges)
+
+    def showMininizedWindow(self):
+        self.setWindowState(Qt.WindowMinimized)
+
+    def closeWindow(self):
+        sys.exit()
+
+    def updateMask(self):
+        frameRect = self.frameGeometry()
+        frame_width = frameRect.width()
+        frame_height = frameRect.height()
+        titleRect = self.winTitle.geometry()
+        title_height = titleRect.height()
+        toolRect = self.toolLayout.geometry()
+        tool_height = toolRect.height()
+
+        trans_height = frame_height-title_height-tool_height
+        trans_width = frame_width-2
+
+        region = QRegion(QRect(0, 0, frameRect.right(), frameRect.bottom()))
+        # "subtract" the grabWidget rectangle to get a mask that only contains
+        # the window titlebar, margins and panel
+        region -= QRegion(QRect(1, titleRect.bottom(), trans_width, trans_height))
+        self.setMask(region)
+
+        self.frameHtLabel.setText(f'高[{trans_height}]像素')
+        self.frameWdLabel.setText(f'宽[{trans_width}]像素')
+
+    def resizeEvent(self, event):
+        super(Helper, self).resizeEvent(event)
+        # the first resizeEvent is called *before* any first-time showEvent and
+        # paintEvent, there's no need to update the mask until then; see below
+        if not self.dirty:
+            self.updateMask()
+
+    def paintEvent(self, event):
+        super(Helper, self).paintEvent(event)
+        # on Linux the frameGeometry is actually updated "sometime" after show()
+        # is called; on Windows and MacOS it *should* happen as soon as the first
+        # non-spontaneous showEvent is called (programmatically called: showEvent
+        # is also called whenever a window is restored after it has been
+        # minimized); we can assume that all that has already happened as soon as
+        # the first paintEvent is called; before then the window is flagged as
+        # "dirty", meaning that there's no need to update its mask yet.
+        # Once paintEvent has been called the first time, the geometries should
+        # have been already updated, we can mark the geometries "clean" and then
+        # actually apply the mask.
+        if self.dirty:
+            self.updateMask()
+            self.dirty = False
 
     def mousePressEvent(self, event):
         self.oldPos = event.globalPos()
+        self.drag = True
 
     def mouseMoveEvent(self, event):
-        delta = QPoint (event.globalPos() - self.oldPos)
-        #print(delta)
-        self.move(self.x() + delta.x(), self.y() + delta.y())
-        self.oldPos = event.globalPos()
+        if event.buttons() == Qt.NoButton:
+            pass
+        elif event.buttons() == Qt.LeftButton and self.drag:
+            delta = QPoint(event.globalPos() - self.oldPos)
+            self.move(self.x() + delta.x(), self.y() + delta.y())
+            self.oldPos = event.globalPos()
 
+    def mouseReleaseEvent(self, event):
+        self.drag = False
+
+    def scriptChanges(self):
+        print(self.scriptChoice.currentText())
+
+    #####################
+    #  Mouse Controller #
+    #####################
+    def updateMouseRelativePos(self):
+        # need to add asyn here
+        x, y = pag.position()
+        time.sleep(0.2)
+
+
+
+class VLine(QFrame):
+    # a simple VLine, like the one you get from designer
+    def __init__(self):
+        super(VLine, self).__init__()
+        self.setFrameShape(self.VLine | self.Sunken)
+        self.setLineWidth(3)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    screen_height = app.primaryScreen().size().height()
+    screen_width = app.primaryScreen().size().width()
     helper = Helper()
     helper.show()
     sys.exit(app.exec_())
